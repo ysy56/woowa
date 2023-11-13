1fb48f31ffcc85511e9e6a4a7b4491dc724acfbf
ysy56 <ysy561356@naver.com>
2023-11-13 20:56:00 +0900

    feat : 플래너 소개

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,14 @@
 package christmas;
 
+
+import view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OutputView outputView = new OutputView();
+
+        outputView.prinEventPlannerMessage();
+
     }
 }
+
---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,7 @@
+package view;
+
+public class OutputView {
+    public void prinEventPlannerMessage() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+}
